3d67545546d07458e1cee8e258b6bc74169283e3
Dmitri <dposkachei@gmail.com>
2018-02-27 23:47:37 +0300

    fix config publish

--- src/Toastr.php
+++ src/Toastr.php
@@ -30,7 +30,7 @@ class Toastr
     protected $config;
 
     private $allowedTypes = ['error', 'info', 'success', 'warning', 'primary'];
-    private $allowedTitles = ['Error', 'Info', 'Success', 'Warning', 'Primary'];
+    private $allowedTitles = ['error', 'Info', 'Success', 'Warning', 'Primary'];
 
     /**
      * Constructor
